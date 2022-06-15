a5bce84a47ad7b112a9b2f92c51f4696047ae99c
bambooozeld <77414753+bambooozeld@users.noreply.github.com>
2022-06-15 14:06:32 +0200

    Main Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SecureFileCorrupter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InfoTextTopLabel = new System.Windows.Forms.Label();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.CorruptFileButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.EncryptFile = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // InfoTextTopLabel
+            // 
+            this.InfoTextTopLabel.AutoSize = true;
+            this.InfoTextTopLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoTextTopLabel.Location = new System.Drawing.Point(55, 9);
+            this.InfoTextTopLabel.Name = "InfoTextTopLabel";
+            this.InfoTextTopLabel.Size = new System.Drawing.Size(313, 16);
+            this.InfoTextTopLabel.TabIndex = 0;
+            this.InfoTextTopLabel.Text = "Corrupt sensitive and important Files savely!";
+            this.InfoTextTopLabel.Click += new System.EventHandler(this.InfoTextTopLabel_Click);
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(58, 337);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(315, 42);
+            this.SelectFileButton.TabIndex = 1;
+            this.SelectFileButton.Text = "Select File";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // CorruptFileButton
+            // 
+            this.CorruptFileButton.Location = new System.Drawing.Point(58, 422);
+            this.CorruptFileButton.Name = "CorruptFileButton";
+            this.CorruptFileButton.Size = new System.Drawing.Size(158, 42);
+            this.CorruptFileButton.TabIndex = 2;
+            this.CorruptFileButton.Text = "Corrupt File";
+            this.CorruptFileButton.UseVisualStyleBackColor = true;
+            this.CorruptFileButton.Click += new System.EventHandler(this.CorruptFileButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(58, 507);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(315, 42);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // EncryptFile
+            // 
+            this.EncryptFile.Location = new System.Drawing.Point(222, 422);
+            this.EncryptFile.Name = "EncryptFile";
+            this.EncryptFile.Size = new System.Drawing.Size(151, 42);
+            this.EncryptFile.TabIndex = 4;
+            this.EncryptFile.Text = "Encrypt FIle";
+            this.EncryptFile.UseVisualStyleBackColor = true;
+            this.EncryptFile.Click += new System.EventHandler(this.EncryptFile_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(3, 33);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(38, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Github";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(432, 564);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.EncryptFile);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CorruptFileButton);
+            this.Controls.Add(this.SelectFileButton);
+            this.Controls.Add(this.InfoTextTopLabel);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Secure File Corrupter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label InfoTextTopLabel;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.Button CorruptFileButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button EncryptFile;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SecureFileCorrupter
+{
+    public partial class Form1 : Form
+    {
+        string filepath_name = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoTextTopLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            GetFPNasString();
+        }
+        public string GetFPNasString()
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            string NoFileSelected = "No File Selected!";
+
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                filepath_name = file.FileName;
+                SelectFileButton.Text = filepath_name;
+                return filepath_name;
+            }
+            else
+            {
+                SelectFileButton.Text = NoFileSelected;
+                return null;
+            }
+        }
+
+        public void CorruptFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to corrupt this file?", "Corrupt Files", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (SelectFileButton.Text != "No File Selected!")
+                {
+                    byte[] fileBytes = File.ReadAllBytes(filepath_name);
+                    StringBuilder sb = new StringBuilder();
+
+                    foreach (byte b in fileBytes)
+                    {
+                        sb.Append(Convert.ToString(b, 2).PadLeft(0, '0'));
+                    }
+                    File.WriteAllText(filepath_name, sb.ToString());
+                    string changedName = Path.ChangeExtension(filepath_name, null);
+                    File.Move(filepath_name, changedName);
+                    DialogResult answer = MessageBox.Show("Do you also want to encrypt this file?", "Encrypt Files", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (answer == DialogResult.Yes)
+                    {
+                        AES_Algo();
+                    }
+                    else if (answer == DialogResult.No)
+                    {
+
+                    }
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }
+        }
+
+        private void EncryptFile_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to encrypt this file?", "Encrypt Files", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (SelectFileButton.Text != "No File Selected!")
+                {
+                    AES_Algo();
+                }
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                
+            }
+        }
+        public void AES_Algo()
+        {
+
+            try
+            {
+                using (FileStream fileStream = new FileStream(filepath_name, FileMode.OpenOrCreate))
+                {
+                    using (Aes aes = Aes.Create())
+                    {
+                        byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
+                        aes.Key = key;
+
+                        byte[] iv = aes.IV;
+                        fileStream.Write(iv, 0, iv.Length);
+
+                        using (CryptoStream cryptoStream = new CryptoStream(fileStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter encryptWriter = new StreamWriter(cryptoStream))
+                            {
+                                encryptWriter.WriteLine("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("Files Encrypted successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Encryption failed: {0}",ex.Message);
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to exit this program? \n" + "Have you saved everything?"
+            , "Exit Program", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/bambooozeld/FileCorrupter");
+        }
+        
+        private void Images(string[] args)
+        {
+            mywindow.Icon = new BitmapImage(new Uri(@"C:\myicon.ico"));
+        }
+    }
+}
